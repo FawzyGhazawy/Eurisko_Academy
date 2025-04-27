33e3cfca032c774a4816fc7b1b8a3afb8f74c6b5
Fawzy Ghazawy <https://github.com/FawzyGhazawy>
2025-04-28 02:23:59 +0300

    fixed dob validation date between year 1900 and today

--- src/forms/EditUserForm.tsx
+++ src/forms/EditUserForm.tsx
@@ -14,8 +14,21 @@ const schema = z.object({
   lastName: z.string().optional(),
   email: z.string().email('Invalid email address'),
   status: z.nativeEnum(StatusEnum), // Use the enum for validation
-  dateOfBirth: z.string().regex(/^\d{4}-\d{2}-\d{2}$/, 'Date must be in YYYY-MM-DD format'),
-});
+  dateOfBirth: z
+  .string()
+  .regex(/^\d{4}-\d{2}-\d{2}$/, 'Date must be in YYYY-MM-DD format')
+  .refine(
+    (dob) => {
+      const dobDate = new Date(dob);
+      const currentDate = new Date();
+      const minDate = new Date('1900-01-01');
+
+      return dobDate <= currentDate && dobDate >= minDate;
+    },
+    {
+      message: 'Date of birth must be between January 1, 1900, and today.',
+    }
+  ),});
 
 type FormData = z.infer<typeof schema>;
 
